de29a90a0ed0702b6e8fd3c4697f5fe20fbe0a0c
iraizo <admin@raizo.dev>
2024-05-03 13:43:20 +0200

    impl backpack.tf api

--- src/sources/mod.rs
+++ src/sources/mod.rs
@@ -1,1 +1,22 @@
 pub mod bptf;
+
+use self::bptf::Listing;
+
+#[derive(Debug)]
+pub enum PricingError {
+    ServerError,
+    InternalError,
+    InvalidConfig,
+}
+
+pub trait PriceSource {
+    fn get_lowest_seller(
+        &self,
+        item: &str,
+    ) -> impl std::future::Future<Output = Result<Listing, PricingError>> + Send;
+
+    fn get_listings(
+        &self,
+        item: &str,
+    ) -> impl std::future::Future<Output = Result<Vec<Listing>, PricingError>> + Send;
+}
